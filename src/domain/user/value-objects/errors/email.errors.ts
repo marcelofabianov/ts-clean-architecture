1989ef9420c0156242b33a4740540f2e9edacf10
Marcelo Fabiano <marcelofabianov@gmail.com>
2022-08-24 16:36:00 -0300

    refactor: errors email

--- src/domain/user/value-objects/errors/email.errors.ts
+++ src/domain/user/value-objects/errors/email.errors.ts
@@ -8,4 +8,18 @@ export const ERROR_EMPTY_EMAIL = {
   message: 'Empty email address'
 };
 
-export type EmailErrors = 'ERROR_INVALID_EMAIL' | 'ERROR_EMPTY_EMAIL';
+export type EmailErrorsType = 'ERROR_INVALID_EMAIL' | 'ERROR_EMPTY_EMAIL';
+
+export const EmailErrors = async (error: EmailErrorsType): Promise<string> => {
+  switch (error) {
+    case ERROR_EMPTY_EMAIL.type: {
+      return ERROR_EMPTY_EMAIL.message;
+    }
+    case ERROR_INVALID_EMAIL.type: {
+      return ERROR_INVALID_EMAIL.message;
+    }
+    default: {
+      return 'error';
+    }
+  }
+};
